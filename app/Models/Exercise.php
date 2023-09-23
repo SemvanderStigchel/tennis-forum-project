644e9142a5cc6a88c353653c784d2c26c6a006d5
SemvanderStigchel <1058693@hr.nl>
2023-09-23 11:32:23 +0200

    First relations specified in models

--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -8,5 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Exercise extends Model
 {
     use HasFactory;
+
+    public function tag(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Tag::class);
+    }
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
 
